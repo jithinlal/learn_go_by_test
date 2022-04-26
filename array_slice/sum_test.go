156a90134b7e1f2f6d2daef303b23f09d7d60971
jithinlalpr <jithinlal.pr@neoito.com>
2022-04-26 23:06:17 +0530

    fix: sum function

---
+++ array_slice/sum_test.go
@@ -0,0 +1,22 @@
+package array_slice
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
+}
